643ada3e120b67a52ef180d68755f821897f0c21
Daniel <daniel.paviza@gmail.com>
2025-06-14 21:41:32 +0200

    application name + icon

--- Models/Credential.cs
+++ Models/Credential.cs
@@ -5,7 +5,9 @@ public class Credential {
     public int Id { get; set; }
     public string ServiceName { get; set; } = string.Empty;
     public string Username { get; set; } = string.Empty;
+    public bool MaskUsername { get; set; } = false;
     public string Password { get; set; } = string.Empty;
+    public bool MaskPassword { get; set; } = true;
     public string Note { get; set; } = string.Empty;
 }
 
